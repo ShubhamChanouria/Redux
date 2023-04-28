fccaf40854ce4a869d6cfbd4b2b56fd827b9d045
vikassics <vikas@joinnextmed.com>
2023-04-29 04:54:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar";
+import UserDetails from "./Components/UserDetails";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <UserDetails />
+    </>
+  );
+}
+
+export default App;
---
+++ src/store/slices/UserSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: [],
+  reducers: {
+    addUSer(state, action) {},
+    removeUser(state, action) {},
+    deleteUsers(state, action){}
+  },
+});
+
+
+console.log(userSlice.actions);
+
+export default userSlice;
+
